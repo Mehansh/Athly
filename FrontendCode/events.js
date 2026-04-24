2e4781985728bc37adab2969d2d8649d47123944
Mehansh <mehumash@gmail.com>
2026-04-24 18:59:42 +0530

    type field added

--- FrontendCode/events.js
+++ FrontendCode/events.js
@@ -344,7 +344,7 @@ function normalizeEventData(data, sourceConfig, docId) {
         organizer: organizerName,
         date: data.date || "Date TBA",
         location: locationStr,
-        type: sourceConfig.type,
+        type: data.type || sourceConfig.type,
         displayType: displayType,
         distance: dist,
         difficulty: difficulty,
